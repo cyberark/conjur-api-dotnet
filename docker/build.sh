019481d0961b7523d47412fb91972aadbbc7c0a8
egvili <eyal.gvili@cyberark.com>
2021-07-21 15:53:43 +0300

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -12,9 +12,8 @@ xbuild
 nunit-console test/test.csproj
 
 # build
-cd conjur-api
 msbuild api-dotnet.sln /t:build /p:Configuration="Release" /p:Platform="Any CPU" /p:SkipInvalidConfigurations=true /m \
   /p:BuildInParallel=true /p:AllowUntrustedCertificate=False /p:CreatePackageOnPublish=False /p:DeployOnBuild=False \
    /p:GenerateVSPropsFile=True /p:NodeReuse=False /p:RunCodeAnalysis=AsConfigured /p:ToolPlatform=Auto /p:Verbosity=Normal
 
-../docker/nuget.sh
+docker/nuget.sh
--- docker/nuget.sh
+++ docker/nuget.sh
@@ -1,12 +1,12 @@
 mkdir nugetPackages
 
-version=`ikdasm -assembly bin/Release/conjur-api.dll | grep -E Version | sed -n -r 's@^Version:\s+([[:digit:]]+\.[[:digit:]]+)\..*@\1@p'`
-nuget pack ./conjur-api.nuspec -Properties configuration="Release" -Properties platform="Any CPU" \
+version=`ikdasm -assembly conjur-api/bin/Release/conjur-api.dll | grep -E Version | sed -n -r 's@^Version:\s+([[:digit:]]+\.[[:digit:]]+)\..*@\1@p'`
+nuget pack conjur-api/conjur-api.nuspec -Properties configuration="Release" -Properties platform="Any CPU" \
           -OutputDirectory ./nugetPackages -Version "${version:-0.0}.${build_name}" -Verbosity detailed
 
 nuget sources add -Name "conjur-api-dotnet" -Source "https://conjurinc.jfrog.io/artifactory/api/nuget/conjur-api-dotnet" \
      -UserName $WRITE_ARTIFACTORY_USERNAME -Password $WRITE_ARTIFACTORY_PASSWORD -Verbosity detailed
 
-nuget push ./nugetPackages/* -Source "conjur-api-dotnet" -Verbosity detailed
+#nuget push ./nugetPackages/* -Source "conjur-api-dotnet" -Verbosity detailed
 
 rm -rf nugetPackages
