b38a682b026f11b1ff4f6c6bef4351fc054673a7
sheigh <shlomo.heigh@cyberark.com>
2023-08-18 16:52:39 +0300

    Upgrade to .NET 6

--- conjur-api/Properties/AssemblyInfo.cs
+++ conjur-api/Properties/AssemblyInfo.cs
@@ -6,20 +6,8 @@
 // </summary>
 using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Conjur.dll")]
-[assembly: AssemblyDescription("Conjur server API library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CyberArk Software Ltd.")]
-[assembly: AssemblyProduct("Conjur .NET API")]
-[assembly: AssemblyCopyright("(c) CyberArk Software Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-/// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-/// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-/// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("2.1.*")]
 
 #if (!SIGNING)
 [assembly: InternalsVisibleTo("ConjurTest")]
