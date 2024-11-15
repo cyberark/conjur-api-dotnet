e8c8f9294056a177371dc3bfda5499d9a439e786
Shlomo Zalman Heigh <shlomo.heigh@cyberark.com>
2024-11-15 11:52:26 -0500

    Allow build script to run locally

--- build.sh
+++ build.sh
@@ -12,6 +12,12 @@ trap finish EXIT
 
 TAG=$(cat docker/tag)
 
+# When running in Jenkins, a VERSION file is automatically added to the repo root.
+# When running locally, add one here.
+if [ ! -f "$PWD/VERSION" ]; then
+  echo "0.0.0-dev" > "$PWD/VERSION"
+fi
+
 CIDFILE=$(mktemp -u)
 docker run -v "$PWD":/src:ro --cidfile="$CIDFILE" -e WRITE_ARTIFACTORY_USERNAME -e WRITE_ARTIFACTORY_PASSWORD -e WRITE_ARTIFACTORY_URL "$TAG"
 
