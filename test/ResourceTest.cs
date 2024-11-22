6be1fafa5741c501653d75e7432451603405b083
Shlomo Zalman Heigh <shlomo.heigh@cyberark.com>
2024-11-22 09:33:10 -0500

    Fix and re-enable tests
    Fixes https://github.com/cyberark/conjur-api-dotnet/issues/46

--- test/ResourceTest.cs
+++ test/ResourceTest.cs
@@ -14,23 +14,19 @@ namespace Conjur.Test
             Client.Authenticator = new MockAuthenticator();
         }
 
-        // TODO: This test has been commented out and will be fixed in Issue #46 (https://github.com/cyberark/conjur-api-dotnet/issues/46)
-        //[Test]
-        //public void TestCheck()
-        //{
-        //    var resource = Client.Resource(Kind, Name);
+        [Test]
+        public void TestCheck()
+        {
+            var resource = Client.Resource(Kind, Name);
 
-        //    var mock = Mocker.Mock(new Uri("test:///resources/" + TestAccount
-        //           + "/" + Kind + "/" + Name + "/?check=true&privilege=fry"), "");
-        //    Assert.IsTrue(resource.Check("fry"));
+            Mocker.Mock(new Uri("test:///resources/" + TestAccount
+                + "/" + Kind + "/" + Name + "/?check=true&privilege=fry"), "");
+            Assert.IsTrue(resource.Check("fry"));
 
-        //    mock.Verifier = (WebRequest) =>
-        //    {
-        //        throw new WebMocker.MockResponseException(
-        //            HttpStatusCode.Forbidden, "Forbidden");
-        //    };
-        //    Assert.IsFalse(resource.Check("fry"));
-        //}
+            Mocker.Mock(new Uri("test:///resources/" + TestAccount
+                    + "/" + Kind + "/" + Name + "/?check=true&privilege=fry"), "", HttpStatusCode.Forbidden);
+            Assert.IsFalse(resource.Check("fry"));
+        }
 
         [Test]
         public void TestNameToId()
